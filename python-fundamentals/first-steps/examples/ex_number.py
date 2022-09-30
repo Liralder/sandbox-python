c00945da11e6b693cf2dc0f92eed3782a2934979
Lucas <antonio.lucas2011@hotmail.com>
2022-09-29 23:06:45 -0300

    Revisão do estudos sobre Números

---
+++ python-fundamentals/first-steps/examples/ex_number.py
@@ -0,0 +1,24 @@
+# Hora de exercitar os conceitos.   #
+print('\nExercitando o conhecimento:')
+#
+print('\nEscrever operações matemáticas básicas que resultem no valor 8:')
+#
+adicao = 4 + 4
+print('Este é um exemplo de adição(+): ' + str(adicao))
+#
+subtracao = 12 - 4
+print('Este é um exemplo de subtração(-): ' + str(subtracao))
+#
+multiplicacao = 2 * 4
+print('Este é um exemplo de multiplicação(*): ' + str(multiplicacao))
+#
+divisao = 16 / 2
+print('Este é um exemplo de divisão(/): ' + str(divisao))
+#
+#
+print('\nArmazene e exiba seu número favorito:')
+#
+favorite_number = 20
+print('Este é meu número favorito: ' + str(favorite_number))
+#
+#
